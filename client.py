f3bba2fffcb55481e55039099f95a08d8ddb80bf
fardeen-gaffoor <fardeengaffoor@gmail.com>
2026-02-25 08:40:26 +0530

    sucess

---
+++ client.py
@@ -0,0 +1,22 @@
+
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(("127.0.0.1", 65432))
+
+done=False
+
+while not done:
+    client.send(input("Message ").encode('utf-8'))
+    msg = client.recv(1024).decode('utf-8')
+
+    if msg == 'quit':
+        done=True
+    else:
+        print(msg)
+
+
+
+client.close()
+client
